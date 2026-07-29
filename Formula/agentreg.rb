db74ce371bb8468a07d9abec0ae7aed8b48f7f17
Austin Kamara Kolleh <momodukamarakolleh0@gmail.com>
2026-07-29 15:59:40 +0000

    agentreg 0.1.2

--- Formula/agentreg.rb
+++ Formula/agentreg.rb
@@ -1,28 +1,28 @@
 class Agentreg < Formula
   desc "DNS for AI agents: register, discover, and health-check MCP agents"
   homepage "https://github.com/mkk2026/agentreg"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/mkk2026/agentreg/releases/download/v0.1.1/agentreg_0.1.1_darwin_arm64.tar.gz"
-      sha256 "0abb7b5445423e266262bd16b02d1e684d005f6194947b28d6024f93e238455a"
+      url "https://github.com/mkk2026/agentreg/releases/download/v0.1.2/agentreg_0.1.2_darwin_arm64.tar.gz"
+      sha256 "f25ad471cc7693a0d8287f3b9e35ecf535a3764696187f18d36a37a2aff409c6"
     end
     on_intel do
-      url "https://github.com/mkk2026/agentreg/releases/download/v0.1.1/agentreg_0.1.1_darwin_amd64.tar.gz"
-      sha256 "edd2642a99b469b754ef41a59f19a44495f7cecc96d4d438517d6bd5a965448b"
+      url "https://github.com/mkk2026/agentreg/releases/download/v0.1.2/agentreg_0.1.2_darwin_amd64.tar.gz"
+      sha256 "80cce4557b39d957d82ae3b0e30c6a608bb2600ee7c06e7e32891ab70702d103"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/mkk2026/agentreg/releases/download/v0.1.1/agentreg_0.1.1_linux_arm64.tar.gz"
-      sha256 "d60cc581c556637d45d1b800ffa0ab87c49bd1dd5f9b8330ef7c7f128f2bac49"
+      url "https://github.com/mkk2026/agentreg/releases/download/v0.1.2/agentreg_0.1.2_linux_arm64.tar.gz"
+      sha256 "42a4e73a7de355d5cb9e86fd00fffebc9a5a01861b5daab003e63cf2667da7de"
     end
     on_intel do
-      url "https://github.com/mkk2026/agentreg/releases/download/v0.1.1/agentreg_0.1.1_linux_amd64.tar.gz"
-      sha256 "28d1df359afd91e6019d311f66dea15f28c433266f98a94333d834007cd41da4"
+      url "https://github.com/mkk2026/agentreg/releases/download/v0.1.2/agentreg_0.1.2_linux_amd64.tar.gz"
+      sha256 "ad788a9720482ddc7c3bd969507ea8fdad98d70870ba036f4881f6cc04439537"
     end
   end
 
@@ -31,6 +31,6 @@ class Agentreg < Formula
   end
 
   test do
-    assert_match "0.1.1", shell_output("#{bin}/agentctl --version")
+    assert_match "0.1.2", shell_output("#{bin}/agentctl --version")
   end
 end
